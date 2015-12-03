08035c0c6f38538f3fa6cdd1c27084df38c1b5ea
1337 <autonomousmachine@gmail.com>
2015-12-03 12:32:03 -0800

    incorporating socket.io

--- components/app.js
+++ components/app.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
+
+	componentWillMount() {
+		this.socket = io('http://localhost:3000');
+		this.socket.on('connect', this.connect);
+	}
+
+	connect() {
+		alert(this.id);
+	}
+
 	render() {
 		return <h1>Hello World from React</h1>;
 	}
